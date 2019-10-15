f4ad7254926225ce5ab49d2edaf4c4992e32e99f
lishaoping <pingshao_li@163.com>
2019-10-15 20:53:48 +0800

    第N次修改entity

--- spring-git/springboot-demo/src/main/java/com/youzhong/entity/user.java
+++ spring-git/springboot-demo/src/main/java/com/youzhong/entity/user.java
@@ -6,6 +6,28 @@ public class user {
     private int age;
     private  String name;
 
+    private String username;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public user(String name) {
         this.name = name;
     }
